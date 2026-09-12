60b777fe56b25911ff8e35f1a5970599abdc8953
esCaseFold <ethansattersten@gmail.com>
2026-09-12 11:50:10 -0700

    Adding files to use GitHub Action to build and deploy project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://esCaseFold.github.io',
+	base: '/SvelteKit-Audited',
 	integrations: [
 		starlight({
 			title: 'SvelteKit Docs Audited',
